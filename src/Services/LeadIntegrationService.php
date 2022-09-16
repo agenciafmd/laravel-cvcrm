187dd2477a11858ca431798f704e3dc654309a18
Carlos Tamagawa <carlos@fmd.ag>
2022-09-16 11:12:51 -0300

    fix(integração) acrescentando o service para poder pegar o id de retorno

---
+++ src/Services/LeadIntegrationService.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Agenciafmd\Cvcrm\Services;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+class LeadIntegrationService
+{
+    public function __construct()
+    {
+    }
+
+    private function getClientRequest()
+    {
+        $logger = new Logger('laravel-cvcrm');
+        $logger->pushHandler(new StreamHandler(storage_path('logs/laravel-cvcrm-' . date('Y-m-d') . '.log')));
+
+        $stack = HandlerStack::create();
+        $stack->push(
+            Middleware::log(
+                $logger,
+                new MessageFormatter("{method} {uri} HTTP/{version} {req_body} | RESPONSE: {code} - {res_body}")
+            )
+        );
+
+        return new Client([
+            'timeout' => 60,
+            'connect_timeout' => 60,
+            'http_errors' => false,
+            'verify' => false,
+            'handler' => $stack,
+        ]);
+    }
+
+    public function sendIntegrationResponse($data = [])
+    {
+        if (!config('laravel-cvcrm.token') || !config('laravel-cvcrm.email') || !config('laravel-cvcrm.url')) {
+            return false;
+        }
+
+        $client = $this->getClientRequest();
+
+        $options = [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'email' => config('laravel-cvcrm.email'),
+                'token' => config("laravel-cvcrm.token")
+            ],
+            'json' => $data,
+        ];
+
+        $response = $client->request('POST', config('laravel-cvcrm.url').'api/cvio/lead', $options);
+
+        $resp = collect(json_decode((string)$response->getBody()));
+
+        if($resp['codigo'] == 200){
+            return $resp;
+        }else{
+            return false;
+        }
+
+
+
+    }
+}
